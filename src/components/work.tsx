7b6cd449d7b2ffe17d96f54d4fafd69f8547544b
omijaia <160079870+omijaia@users.noreply.github.com>
2024-02-28 11:15:39 -0300

    chore: deploy website with development preview

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -4,9 +4,9 @@ import { workContainer, workSquare } from '../styles/work.css';
 const Work = () => {
   return (
     <div className={workContainer}>
-      <div className={workSquare}>Cuadrado 1</div>
-      <div className={workSquare}>Cuadrado 2</div>
-      <div className={workSquare}>Cuadrado 3</div>
+      <div className={workSquare}>Square 1</div>
+      <div className={workSquare}>Square 2</div>
+      <div className={workSquare}>Square 3</div>
     </div>
   );
 };
